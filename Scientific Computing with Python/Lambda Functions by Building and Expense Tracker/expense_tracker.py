5973cb22714d2c936159e5146a877b376997f83f
yonrasgg <geovanny.alpizar@gmail.com>
2024-01-11 20:08:38 -0600

    modified:   Scientific Computing with Python/Lambda Functions by Building and Expense Tracker/expense_tracker.py

--- Scientific Computing with Python/Lambda Functions by Building and Expense Tracker/expense_tracker.py
+++ Scientific Computing with Python/Lambda Functions by Building and Expense Tracker/expense_tracker.py
@@ -10,7 +10,24 @@ def main():
         print('3. Show total expenses')
         print('4. Filter expenses by category')
         print('5. Exit')
+        
         choice = input('Enter your choice: ')
+        if choice == '1':
+            amount = float(input('Enter amount: '))
+            category = input('Enter category: ')
+            add_expense(expenses, amount, category)
+        elif choice == '2':
+            print('\nAll Expenses:')
+            print_expenses(expenses)
+        elif  choice == '3':
+            print('\nTotal Expenses: ', total_expenses(expenses))
+        elif choice == '4':
+            category = input('Enter category to filter: ')
+            print(f'\nExpenses for {category}:')
+            expenses_from_category = filter_expenses_by_category(expenses, category)
+            print_expenses(expenses_from_category)
+        elif choice == '5':
+            print('Exiting the program.')
         
 def print_expenses(expenses):
     for expense in expenses:
@@ -23,3 +40,5 @@ def total_expenses(expenses, test = lambda x: x * 2):
 def filter_expenses_by_category(expenses, category):
     filter(lambda expense: expense['category'] == category, expenses)
     return filter(lambda expense: expense['category'] == category, expenses)
+
+main()
