86e0d37b639476fc3cab5a2baef4d5d1686ec0d5
yonrasgg <geovanny.alpizar@gmail.com>
2024-01-16 00:11:28 -0600

    Fix bug in login functionality

--- Scientific Computing with Python/Regular Expressions by Building a Password Generator/password_generator.py
+++ Scientific Computing with Python/Regular Expressions by Building a Password Generator/password_generator.py
@@ -1,5 +1,11 @@
+import secrets
 import string
 
+# Define the possible characters for the password.
 letters = string.ascii_letters
 digits = string.digits
 symbols = string.punctuation
+# Combine all characters.
+all_characters = letters + digits + symbols
+print(all_characters)
+print(secrets.choice(all_characters))
