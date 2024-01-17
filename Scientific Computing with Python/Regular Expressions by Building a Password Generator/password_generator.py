4da119411941d88cd8ae9271f65fd0e75d418a11
yonrasgg <geovanny.alpizar@gmail.com>
2024-01-16 20:11:05 -0600

    Refactor code to improve performance and readability

--- Scientific Computing with Python/Regular Expressions by Building a Password Generator/password_generator.py
+++ Scientific Computing with Python/Regular Expressions by Building a Password Generator/password_generator.py
@@ -1,11 +1,20 @@
 import secrets
 import string
 
-# Define the possible characters for the password.
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
-# Combine all characters.
-all_characters = letters + digits + symbols
-print(all_characters)
-print(secrets.choice(all_characters))
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+    return password
+            
+# new_password = generate_password(8)
+# print(new_password)
