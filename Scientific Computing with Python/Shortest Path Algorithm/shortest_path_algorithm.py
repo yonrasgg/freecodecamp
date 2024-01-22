d236c0cc3821959ddc1f15c388836cd9adbfd8d3
yonrasgg <geovanny.alpizar@gmail.com>
2024-01-21 18:39:01 -0600

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better readability
    
    Update error handling in API calls
    
    Improve performance of database queries
    
    Update README with installation instructions
    
    Fix formatting issues in CSS
    
    Add missing comments for clarity
    
    Remove unused variables
    
    Update dependencies to latest versions

---
+++ Scientific Computing with Python/Shortest Path Algorithm/shortest_path_algorithm.py
@@ -0,0 +1,20 @@
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+}
+
+def shortest_path(graph, start):
+    unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')
+
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    
+shortest_path(my_graph, 'A')
