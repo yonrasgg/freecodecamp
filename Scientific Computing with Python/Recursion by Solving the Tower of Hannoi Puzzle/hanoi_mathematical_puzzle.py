27cf2690053d5352e92eaf75a1b0f8a2b5e7f3ef
yonrasgg <geovanny.alpizar@gmail.com>
2024-02-03 12:33:43 -0600

    modified:   Scientific Computing with Python/Recursion by Solving the Tower of Hannoi Puzzle/hanoi_mathematical_puzzle.py

--- Scientific Computing with Python/Recursion by Solving the Tower of Hannoi Puzzle/hanoi_mathematical_puzzle.py
+++ Scientific Computing with Python/Recursion by Solving the Tower of Hannoi Puzzle/hanoi_mathematical_puzzle.py
@@ -7,5 +7,40 @@ rods = {
     'C': []
 }
 
-def move():
-    pass
+def make_allowed_move(rod1, rod2):    
+    forward = False
+    if not rods[rod2]:
+        forward = True
+    elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
+        forward = True              
+    if forward:
+        print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+        rods[rod2].append(rods[rod1].pop())
+    else:
+        print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+        rods[rod1].append(rods[rod2].pop())
+        
+# display our progress
+print(rods)
+    
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        else:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+    
+# initiate call from source A to target C with auxiliary B.
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
