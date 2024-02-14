77e79efec298fdeae0c3e3cd0841930246940e0d
yonrasgg <geovanny.alpizar@gmail.com>
2024-02-14 05:38:33 -0600

    class Board creation

---
+++ Scientific Computing with Python/Classes and Objects by Building a Sudoku Solver/sudoku_solver.py
@@ -0,0 +1,13 @@
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def __str__(self):
+        upper_lines = f'\n╔═══{"╤═══"*2}{"╦═══"}{"╤═══"*2}{"╦═══"}{"╤═══"*2}╗\n'
+        middle_lines = f'╟───{"┼───"*2}{"╫───"}{"┼───"*2}{"╫───"}{"┼───"*2}╢\n'
+        lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
+        board_string = upper_lines
+        for index, line  in enumerate(self.board):
+            row_list = []
+            for square_no, part in enumerate():
+              pass
