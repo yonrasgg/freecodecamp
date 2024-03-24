fbc4a8315bb9ea3ec9d5480468e54c4f62b5967b
yonrasgg <geovany.alpizar@gmail.com>
2024-03-24 19:57:39 +0000

    modified:   Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py

--- Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
+++ Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
@@ -1,3 +1,19 @@
 class TreeNode:
-    def __init__():
-        pass
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+        
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:     
+            node.left = self._insert(node.left, key)
+            
