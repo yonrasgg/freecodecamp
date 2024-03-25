5a6e6aee1f82cdc0cdfce2c516e94a5ae101c2c3
yonrasgg <geovany.alpizar@gmail.com>
2024-03-25 00:26:10 +0000

    Add search functionality to BinarySearchTree class

--- Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
+++ Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
@@ -16,4 +16,14 @@ class BinarySearchTree:
             return TreeNode(key)
         if key < node.key:     
             node.left = self._insert(node.left, key)
-            
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
+    
+    def search(self, key):
+        self._search(self.root, key)
+        return self._search(self.root, key)
+    
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
