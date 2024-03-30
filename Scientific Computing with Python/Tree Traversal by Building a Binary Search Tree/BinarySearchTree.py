14bdf8a8197d9a5663817aaabf5058a1599932d2
yonrasgg <geovany.alpizar@gmail.com>
2024-03-30 13:42:05 +0000

    Add __str__ method to TreeNode and update inorder_traversal method in BinarySearchTree

--- Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
+++ Scientific Computing with Python/Tree Traversal by Building a Binary Search Tree/BinarySearchTree.py
@@ -4,6 +4,9 @@ class TreeNode:
         self.left = None
         self.right = None
         
+    def __str__(self):
+        return str(self.key)
+        
 class BinarySearchTree:
     def __init__(self):
         self.root = None
@@ -60,4 +63,21 @@ class BinarySearchTree:
     def inorder_traversal(self):
         result = []
         self._inorder_traversal(self.root, result)
-        
+        return result
+    
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.key)
+            self._inorder_traversal(node.right, result)      
+            
+bst = BinarySearchTree()
+nodes = [50, 30, 20, 40, 70, 60, 80]
+
+for node in nodes:
+    bst.insert(node)
+print("Inorder traversal:", bst.inorder_traversal())
+print("Search for 40:", bst.search(40))
+bst.delete(40)
+print("Inorder traversal after deleting 40:", bst.inorder_traversal())
+print("Search for 40:", bst.search(40))
