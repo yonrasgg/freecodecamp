dc9fd1fd6ccd8a16be13ac0263f0d2d43e99009d
yonrasgg <geovany.alpizar@gmail.com>
2024-03-31 14:52:40 +0000

    Refactor arithmetic_arranger function to handle errors and display results

--- Scientific Computing with Python/Arithmetic Formatter/ArithmeticFormatter.py
+++ Scientific Computing with Python/Arithmetic Formatter/ArithmeticFormatter.py
@@ -1,23 +1,30 @@
-import re
+def arithmetic_arranger(problems, solve=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
-
-def arithmetic_arranger(problems):
     first = ""
     second = ""
     lines = ""
-    sufix = ""
-    string = ""
+    results = ""
     for problem in problems:
-        if (re.search("[^\s0-9.+-]", problem)):
-            if (re.search("[/]", problem) or re.search("[*]", problem)):
-                return "Error: Operator must be '+' or '-'."
+        parts = problem.split()
+        if not parts[0].isdigit() or not parts[2].isdigit():
+            return "Error: Numbers must only contain digits."
+        if len(parts[0]) > 4 or len(parts[2]) > 4:
             return "Error: Numbers cannot be more than four digits."
-        
-        first_number = problem.split(" ")[0]
-        operator = problem.split(" ")[1]
-        secod_number = problem.split(" ")[2]
-        
-        if (len(first_number) >= 5 or len(secod_number) >= 5):
-            return "Error: Numbers cannot be more than four digits."
+        if parts[1] not in ['+', '-']:
+            return "Error: Operator must be '+' or '-'."
 
+        result = str(eval(problem))
+        width = max(len(parts[0]), len(parts[2])) + 2
+        first += parts[0].rjust(width) + '    '
+        second += parts[1] + parts[2].rjust(width - 1) + '    '
+        lines += '-' * width + '    '
+        if solve:
+            results += result.rjust(width) + '    '
+
+    arranged_problems = first.rstrip() + '\n' + second.rstrip() + '\n' + lines.rstrip()
+    if solve:
+        arranged_problems += '\n' + results.rstrip()
     return arranged_problems
+            
