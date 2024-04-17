e40f9411d061ebfe703a73cf0f30bcd185f9b715
yonrasgg <geovanny.alpizar@gmail.com>
2024-04-17 21:12:52 +0000

    Add get_balance and transfer methods to Category class

--- Scientific Computing with Python/Budget App/budget_app.py
+++ Scientific Computing with Python/Budget App/budget_app.py
@@ -12,12 +12,21 @@ class Category:
     def withdraw(self, amount, description = " "):
         if self.check_funds(amount):
             if description == None:
-                self.ledger.append({"amount": -amount, "description": ' '})
+                self.ledger.append({"amount": - amount, "description": ' '})
             else:
-                self.ledger.append({'amount': -(amount), 'description': description})         
+                self.ledger.append({'amount': - amount, 'description': description})         
             return True
         else:
             return False
+        
+    def get_balance(self):
+        total_deposits = sum(transaction['amount'] for transaction in self.ledger if transaction['amount'] > 0)
+        total_withdrawals = sum(transaction['amount'] for transaction in self.ledger if transaction['amount'] < 0)
+        balance = total_deposits + total_withdrawals
+        return balance
+    
+    def transfer(self, amount, category):
+        
         
 def create_spend_chart(categories):
     pass
