a605743a986f3146eccca594a5e02f21323c9408
yonrasgg <geovanny.alpizar@gmail.com>
2024-04-18 17:30:25 +0000

    Fix formatting and logic issues in Category class

--- Scientific Computing with Python/Budget App/budget_app.py
+++ Scientific Computing with Python/Budget App/budget_app.py
@@ -1,11 +1,12 @@
 class Category:
+    
     def __init__(self, name):
         self.name = name 
-        self.ledger = []  # list of varables
+        self.ledger = list()
     
     def deposit(self, amount, description = " "):
         if description == None:
-            self.ledger.append({"amount": amount, "description": ' '})
+            self.ledger.append({"amount": amount, "description": " "})
         else:
             self.ledger.append({"amount": amount, "description": description})
             
@@ -20,13 +21,17 @@ class Category:
             return False
         
     def get_balance(self):
-        total_deposits = sum(transaction['amount'] for transaction in self.ledger if transaction['amount'] > 0)
-        total_withdrawals = sum(transaction['amount'] for transaction in self.ledger if transaction['amount'] < 0)
-        balance = total_deposits + total_withdrawals
-        return balance
+        cash_available = 0
+        for item in self.ledger:
+            cash_available += item['amount']
     
     def transfer(self, amount, category):
-        
-        
+        if withdraw:
+            withdraw = self.withdraw(amount, f"Transfer to {category.name}")
+            return True
+        else:
+            category.deposit(amount, f"Transfer from {self.name}")
+            return False
+                
 def create_spend_chart(categories):
     pass
