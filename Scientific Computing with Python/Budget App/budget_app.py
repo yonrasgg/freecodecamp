52d6f327220f5d17ac3f105862281b9d97bfc9a5
yonrasgg <geovanny.alpizar@gmail.com>
2024-04-19 23:40:35 +0000

    Fix formatting and logic issues in Category class

--- Scientific Computing with Python/Budget App/budget_app.py
+++ Scientific Computing with Python/Budget App/budget_app.py
@@ -7,12 +7,12 @@ class Category:
         self.name = name 
         self.ledger = list()
     
-    def deposit(self, amount, description = " "):
+    def deposit(self, amount, description = ""):
         self.ledger.append({"amount": amount, "description": description})
             
-    def withdraw(self, amount, description = " "):
+    def withdraw(self, amount, description = ""):
         if self.check_funds(amount):
-            self.ledger.append({'amount': - amount, 'description': description})         
+            self.ledger.append({'amount': - amount, "description": description})         
             return True
         return False
         
@@ -25,7 +25,7 @@ class Category:
     
     def transfer(self, amount, category):
         if self.check_funds(amount):
-             self.withdraw(-amount, f'Transfer to {category.name}')
+             self.withdraw(amount, f'Transfer to {category.name}')
              category.deposit(amount, f'Transfer from {self.name}')
              return True
         return False
@@ -51,19 +51,55 @@ class Category:
         output = title + items + 'Total: ' + str(total)
         return output    
     
+    @staticmethod
     def truncate(n):
         multiplier = 10
         return int(n * multiplier) / multiplier
-    
+
+    @staticmethod
     def get_totals(categories):
         total = 0
         breakdown = []
         for category in categories:
             total += category.get_withdrawals()
-        rounded = list(map(lambda x: truncate(x / total), breakdown))
+            breakdown.append(category.get_withdrawals())
+        rounded = list(map(lambda x: Category.truncate(x / total), breakdown))
         return rounded
                 
 def create_spend_chart(categories):
     res = 'Percentage spent by category\n'
     i = 100
-    totals = getTotals(categories)
+    totals = Category.get_totals(categories)
+    while i >= 0:
+        cat_spaces = ' '
+        for total in totals:
+            if total * 100 >= i:
+                cat_spaces += 'o  '
+            else:
+                cat_spaces += '   '
+        res += str(i).rjust(3) + '|' + cat_spaces + ('\n')
+        i -= 10
+        
+    dashes = '-' + '---'*len(categories)
+    names = []
+    x_axis = ''
+    for category in categories:
+        names.append(category.name)
+        
+    maxi = max(names, key=len)
+    
+    for x in range(len(maxi)):
+        nameStr = '     '
+        for name in names:
+            if x >= len(name):
+                nameStr += '   '
+            else:
+                nameStr += name[x] + '  '
+        if x != len(maxi)-1:
+            nameStr += '\n'
+            
+        x_axis += nameStr
+        
+    res += dashes.rjust(len(dashes)+4) + '\n' + x_axis
+    return res
+    
