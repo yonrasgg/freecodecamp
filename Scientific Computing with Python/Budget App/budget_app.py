8fb8c886b5daffb95eb46351738e38ca4cfdabda
yonrasgg <geovanny.alpizar@gmail.com>
2024-04-19 22:41:56 +0000

    Add create_spent_chart function to budget_app.py

--- Scientific Computing with Python/Budget App/budget_app.py
+++ Scientific Computing with Python/Budget App/budget_app.py
@@ -1,3 +1,6 @@
+def create_spent_chart(categories):
+    return None
+
 class Category:
     
     def __init__(self, name):
@@ -5,40 +8,41 @@ class Category:
         self.ledger = list()
     
     def deposit(self, amount, description = " "):
-        if description == None:
-            self.ledger.append({"amount": amount, "description": " "})
-        else:
-            self.ledger.append({"amount": amount, "description": description})
+        self.ledger.append({"amount": amount, "description": description})
             
     def withdraw(self, amount, description = " "):
         if self.check_funds(amount):
-            if description == None:
-                self.ledger.append({"amount": - amount, "description": ' '})
-            else:
-                self.ledger.append({'amount': - amount, 'description': description})         
+            self.ledger.append({'amount': - amount, 'description': description})         
             return True
-        else:
-            return False
+        return False
         
     def get_balance(self):
         cash_available = 0
         for item in self.ledger:
             cash_available += item['amount']
+            
+        return cash_available
     
     def transfer(self, amount, category):
-        if self.check_funds(amount) == True:
-             self.ledger.append({"amount": - amount, "description": f'Transfer to {category.name}'})
-             category.deposit(amount, description = f'Transfer from {self.name}')
+        if self.check_funds(amount):
+             self.withdraw(-amount, f'Transfer to {category.name}')
+             category.deposit(amount, f'Transfer from {self.name}')
              return True
-        else:
-            return False
+        return False
         
     def check_funds(self, amount):
         return amount <= self.get_balance()
     
     def __str__(self):
-        
-    
+        title = f'{self.name:*^30}\n'
+        items = ''
+        total = 0
+        for item in self.ledger:
+            items += f'{item["description"][0:23]:23}' + f'{item["amount"]:>7.2f}' + '\n'
+            total += item['amount'] 
+            
+        output = title + items + 'Total: ' + str(total)
+        return output    
                 
 def create_spend_chart(categories):
     pass
